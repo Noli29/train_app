8625b2deec8ffaeb9cba9575b1229764c1dee023
Noli29 <noli.pogorelova@gmail.com>
2016-05-28 22:11:36 +0300

    update

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,6 +1,34 @@
 class TrainsController < ApplicationController
 
   def index
-    @tickets = Ticket.order("train_number").page(params[:page]).per(4)
+    @trains = Train.order("train_number").page(params[:page]).per(4)
+  end
+
+
+  def show
+    @train = Train.find(params[:id])
+  end
+
+  def new
+    @train = Train.new
+  end
+
+  def create
+    @train = Train.new(params[:train])
+    if @train.save
+      session[:train_number] = @train_number
+      flash[:success] = ""
+      redirect_to @train
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+
+  def train_params
+    params.require(:train).permit(:train_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
 
-  #root 'welcome#index'
+  root 'welcome#index'
 
-  #resources :tickets
- # resources :trains
+  resources :tickets
+  resources :trains
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
