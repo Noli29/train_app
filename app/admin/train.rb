a8cbde655dda06b1fa22122f3e3c790f0c5bb10f
valik-pc <valik.v1per@gmail.com>
2016-05-29 13:16:48 +0300

    update show train page

--- app/admin/train.rb
+++ app/admin/train.rb
@@ -1,22 +1,44 @@
 ActiveAdmin.register Train do
-    permit_params :train_number, carriages_attributes: [:carriage, :_destroy, places_attributes: [:place, :_destroy]]
+  permit_params :train_number, carriages_attributes: [:carriage, :_destroy, places_attributes: [:place, :_destroy]]
 
-    config.filters = false
+  config.filters = false
 
-    form do |f|
-        f.inputs do
-            f.input :train_number
-            f.has_many :carriages, heading: 'Вагоны',  new_record: true do |c|
-                c.input :carriage, label: 'Номер вагона'
-
-                if !c.object.nil?
-                    c.has_many :places, heading: 'Місця', new_record: true do |p|
-                        p.input :place, label: 'Номер місця'
-                    end
+  show do
+    h3 'Номер потягу: ' << train.train_number.to_s
+    div do
+        train.carriages.map do |c|
+          attributes_table_for c do
+            row "Вагон:" do
+              c.carriage
+            end
+            div do
+              c.places.map do |p|
+                attributes_table_for p do
+                  row "+Місце:" do
+                    p.place
+                  end
                 end
+              end
             end
         end
-        f.actions
+      end
     end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :train_number, label: 'Номер поезда'
+      f.has_many :carriages, heading: 'Вагоны', new_record: true do |c|
+        c.input :carriage, label: 'Номер вагона'
+
+        if !c.object.nil?
+          c.has_many :places, heading: 'Місця', new_record: true do |p|
+            p.input :place, label: 'Номер місця'
+          end
+        end
+      end
+    end
+    f.actions
+  end
 
 end
